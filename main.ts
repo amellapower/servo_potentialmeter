05a80b0659b353f3f5c7fbfef4f9d8c7981f700e
amellapower <39852391+amellapower@users.noreply.github.com>
2022-12-15 15:34:42 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P14, pins.map(
+    pins.analogReadPin(AnalogPin.P3),
+    0,
+    1023,
+    0,
+    180
+    ))
+    pins.servoWritePin(AnalogPin.P15, pins.map(
+    pins.analogReadPin(AnalogPin.P3),
+    0,
+    1023,
+    0,
+    180
+    ))
+})
